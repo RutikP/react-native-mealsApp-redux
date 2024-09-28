f14e7f232a50c92bf73c4fb333382267282551ea
RutikP <61121027+RutikP@users.noreply.github.com>
2024-09-28 12:58:52 +0530

    Create favorites.js

---
+++ store/redux/favorites.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favoriteSlice = createSlice({
+    name: 'favorites',
+    initialState: {
+        ids: []
+    },
+    reducers: {
+        addFavorite: (state, action) => {
+            state.ids.push(action.payload.id);
+        },
+        removeFavorite: (state, action) => {
+            state.ids.slice(state.ids.indexOf(action.payload.id), 1);
+        }
+    }
+});
+export const addFavorite = favoriteSlice.actions.addFavorite;
+export const removeFavorite = favoriteSlice.actions.removeFavorite;
+export default favoriteSlice.reducer;
